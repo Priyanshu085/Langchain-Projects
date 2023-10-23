2f40484c02dc8397bcc11744ce737bd3133c2669
Priyanshu085 <abpriyanshu007@gmail.com>
2023-10-24 00:08:04 +0530

    Day 2

--- README_File_Generator.py
+++ README_File_Generator.py
@@ -12,34 +12,38 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 llm = OpenAI()
 chat_model = ChatOpenAI()
 
+import streamlit as st
+
 # Input the Required Values
-name = input("Enter The Project Name")
-tech = eval(input("Enter the Tech Stack used in the Project"))
-desc = input("Write a breif about your project")
+name = st.text_input("Enter The Project Name")
+tech = st.multiselect("Enter the Tech Stack used in the Project", ['ReactJS','NextJS','MongoDB','NodeJS','JavaScript','TypeScript'])
+desc = st.text_input("Write a breif about your project")
 
-my_template = '''
-Project name {name}.
-Tech Stack is/are {tech}.
-A Breif desription about my project is {desc}.
+if st.button('Generate README file'):
+  if ( name and tech and desc ):
+    my_template = '''
+    Project name {name}.
+    Tech Stack is/are {tech}.
+    A Breif desription about my project is {desc}.
 
-\'\'\'Include the columns in following order in the README File.\'\'\'
-- Table of Content
-- Screenshot of project
-- Description (in 50 words)
-- Features (in 5 Bullet Points)
-- Technology Used
-- Usage
-- Deployment
-- License 
+    \'\'\'Include the columns in following order in the README File.\'\'\'
+    - Table of Content
+    - Screenshot of project
+    - Description (in 50 words)
+    - Features (in 5 Bullet Points)
+    - Technology Used
+    - Usage
+    - Deployment
+    - License 
 
-\* Make the Deployment and License part as optional for the user
+    \* Make the Deployment and License part as optional for the user
 
-Create a README file in markdown. Also fill the details appropriately.
+    Create a README file in markdown. Also fill the details appropriately.
 
-'''
+    '''
 
-prompt_template = ChatPromptTemplate.from_template(template=my_template)
+    prompt_template = ChatPromptTemplate.from_template(template=my_template)
 
-chain = LLMChain(llm=llm, prompt=prompt_template)
+    chain = LLMChain(llm=llm, prompt=prompt_template)
 
-chain.predict()
+    st.markdown(chain.predict(name=name, desc=desc, tech=tech))
